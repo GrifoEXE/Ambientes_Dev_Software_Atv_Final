d3692785c53eade20600bdbaf11456d14e2f0553
JotaMacLane <jpnascimentoa@gmail.com>
2023-06-01 20:32:55 -0300

    Código de Multiplicação sendo implementada.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
 import operations.Divisao;
+import operations.Multiplicacao;
 
 public class Main {
     public static void main(String[] args) {
         Divisao divisao = new Divisao();
+        Multiplicacao multiplicacao = new Multiplicacao();
         System.out.println(divisao.divide(1, 2));
+        System.out.print(multiplicacao.multi(5,4));
+
     }
 }
