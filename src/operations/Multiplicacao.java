696331dfdda2125e34d95f3182bc1fd52bc774c7
JotaMacLane <jpnascimentoa@gmail.com>
2023-06-01 20:36:08 -0300

    Código de Multiplicação sendo implementada.

---
+++ src/operations/Multiplicacao.java
@@ -0,0 +1,13 @@
+package operations;
+
+public class Multiplicacao {
+    public int multi(int x, int y){
+        return  x * y;
+    }
+    public int fatorial(int x){
+        if(x==0) {
+            return 1;
+        }
+        return x * fatorial(x-1);
+    }
+}
