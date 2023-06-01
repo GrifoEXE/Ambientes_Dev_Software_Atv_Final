faf72dc56c4055e96af6679fc267b985126217ee
Pedro Figueiredo <pedroernestofsf@gmail.com>
2023-06-01 10:30:32 -0300

    refatoração de código

--- src/operations/Soma.java
+++ src/operations/Soma.java
@@ -3,11 +3,11 @@ package operations;
 import java.util.List;
 
 public class Soma {
-    public static double somarDoisValores(Double valor1, Double valor2) {
+    public double somarDoisValores(Double valor1, Double valor2) {
         return valor1 + valor2;
     }
 
-    public static double somarListaDeValores(List<Double> valores) {
+    public double somarListaDeValores(List<Double> valores) {
         return valores.stream().reduce(Double::sum).orElse(0.0);
     }
 }
