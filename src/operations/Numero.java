1ff568d02e51b518021b7c0e69731363d2f53fc9
raulteles27 <134647715+raulteles27@users.noreply.github.com>
2023-06-06 00:57:31 -0300

    Create Numero.java

---
+++ src/operations/Numero.java
@@ -0,0 +1,13 @@
+public class numero {
+    
+    private int valor;
+    public Numero();{
+        this.valor=0
+}
+public void setValor(Int valor){
+    Int.valor=valor
+}
+public int getValor() {
+    return this.valor;
+}
+}
