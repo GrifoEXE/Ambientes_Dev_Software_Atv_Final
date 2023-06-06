2167ce8de7a7a7bd573f74b415066dbad838a89c
Miqueias Pires <miqueiasousa011@gmail.com>
2023-06-06 20:03:31 -0300

    add metodo para calcular a pontencia, dado um numero e um expoente

--- src/operations/Soma.java
+++ src/operations/Soma.java
@@ -10,4 +10,17 @@ public class Soma {
     public double somarListaDeValores(List<Double> valores) {
         return valores.stream().reduce(Double::sum).orElse(0.0);
     }
+    
+    public double potencia(int numero, int expoente) {
+        var mult = new Multiplicacao();
+        int resultado = 1;
+        int count = 0;
+
+        while (count < expoente) {
+            count++;
+            resultado = mult.multi(numero, resultado);
+        }
+
+        return resultado;
+    }
 }
