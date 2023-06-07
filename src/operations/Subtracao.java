b099c18f37b0f5f26d35b9eca3842672f9d94cd1
raulteles27 <134647715+raulteles27@users.noreply.github.com>
2023-06-07 14:26:30 -0300

    Update Subtracao.java

--- src/operations/Subtracao.java
+++ src/operations/Subtracao.java
@@ -1,16 +1,24 @@
+
+
 package operations;
 
 public class Subtracao {
-
-    Soma soma = new Soma();
-
-    public double subtrairDoisValores(double valor1, double valor2) {
-        return valor1 - valor2;
+Soma soma = new Soma ();
+public double subtrairDoisValores (double valor1, double valor2) {
+return valor1 - valor2;
+}
+public double subtrairSomaDeValores (double valor1, double valor2, double valor3, double valor4) {
+valor1 = valor1 + valor2;
+valor3 = valor3 + valor4;
+return soma.somarDoisValores (valor1, valor3);
     }
 
-    public double subtrairSomaDeValores(double valor1, double valor2, double valor3, double valor4) {
-        valor1 = valor1 + valor2;
-        valor3 = valor3 + valor4;
-        return soma.somarDoisValores(valor1, valor3);
+    public static int divideSubtracao(int valor1, int valor2) {
+        int subtracao = valor1 - valor2;
+        int resultado = valor1 / subtracao;
+        return resultado;
     }
 }
+
+
+
